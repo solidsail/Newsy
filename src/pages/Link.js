86a87ba14963423efe208849b70b84428a3b63a3
Bharat Kilaru <bkkilaru@gmail.com>
2020-04-09 15:09:59 -0700

    Link Comments

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -13,12 +13,15 @@ import {
 import NavHeader from "../components/Header/NavHeader";
 import { closeCircleOutline } from "ionicons/icons";
 import LinkItem from "../components/Link/LinkItem";
+import CommentModal from "../components/Link/CommentModal";
+import LinkComment from "../components/Link/LinkComment";
 
 const { Browser } = Plugins;
 
 const Link = (props) => {
   const { user } = React.useContext(UserContext);
   const [link, setLink] = React.useState(null);
+  const [showModal, setShowModal] = React.useState(false);
   const linkId = props.match.params.linkId;
   const linkRef = firebase.db.collection("links").doc(linkId);
 
@@ -31,6 +34,42 @@ const Link = (props) => {
     linkRef.get().then((doc) => {
       setLink({ ...doc.data(), id: doc.id });
     });
+  }
+
+  function handleOpenModal() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      setShowModal(true);
+    }
+  }
+
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+
+  function handleAddComment(commentText) {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      linkRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousComments = doc.data().comments;
+          const newComment = {
+            postedBy: { id: user.uid, name: user.displayName },
+            created: Date.now(),
+            text: commentText,
+          };
+          const updatedComments = [...previousComments, newComment];
+          linkRef.update({ comments: updatedComments });
+          setLink((prevState) => ({
+            ...prevState,
+            comments: updatedComments,
+          }));
+        }
+      });
+      setShowModal(false);
+    }
   }
 
   function handleAddVote() {
@@ -85,6 +124,12 @@ const Link = (props) => {
         action={handleDeleteLink}
       />
       <IonContent>
+        <CommentModal
+          isOpen={showModal}
+          title="New Comment"
+          sendAction={handleAddComment}
+          closeAction={handleCloseModal}
+        />
         {link && (
           <>
             <IonGrid>
@@ -94,9 +139,20 @@ const Link = (props) => {
                   <IonButton onClick={() => handleAddVote()} size="small">
                     Upvote
                   </IonButton>
+                  <IonButton onClick={() => handleOpenModal()} size="small">
+                    Comment
+                  </IonButton>
                 </IonCol>
               </IonRow>
             </IonGrid>
+            {link.comments.map((comment, index) => (
+              <LinkComment
+                key={index}
+                comment={comment}
+                link={link}
+                setLink={setLink}
+              />
+            ))}
           </>
         )}
       </IonContent>
