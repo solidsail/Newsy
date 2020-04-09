454fedfd306803d2a1b61ecbf58fe8be439a8366
Bharat Kilaru <bkkilaru@gmail.com>
2020-04-08 22:37:04 -0700

    Firebase Authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,28 @@ class Firebase {
     this.auth = app.auth();
     this.db = app.firestore();
   }
+
+  async register(name, email, password) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    return newUser.user.updateProfile({
+      displayName: name,
+    });
+  }
+
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  resetPassword(email) {
+    return this.auth.sendPasswordResetEmail(email);
+  }
 }
 
 const firebase = new Firebase();
